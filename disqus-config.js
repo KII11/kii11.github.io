20f06932b177bba63e2186b170bf70509b43793b
kii11 <riskiferdiansyah35@gmail.com>
2023-09-16 03:00:37 +0700

    perubahan1

---
+++ disqus-config.js
@@ -0,0 +1,10 @@
+/**
+ * RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+ * LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+ */
+/*
+var disqus_config = function () {
+  this.page.url = https://https-kii11-github-io-kii-halaman4-html;  // Ganti PAGE_URL dengan URL halaman Anda
+  this.page.identifier = KOMENTAR; // Ganti PAGE_IDENTIFIER dengan identifier unik halaman Anda
+};
+*/
---
+++ disqus.js
@@ -0,0 +1,6 @@
+(function() {
+  var d = document, s = d.createElement('script');
+  s.src = 'https://https-kii11-github-io-kii-halaman4-html.disqus.com/embed.js'; // Ganti dengan URL situs Disqus Anda
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
