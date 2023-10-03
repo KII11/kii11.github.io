231ce6401d9d4b03334e8e1478637af0f7c6ef6c
kian <145009421+KII11@users.noreply.github.com>
2023-10-03 14:28:38 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -115,3 +115,9 @@ retryButton.addEventListener('click', () => {
 });
 
 displayQuestion();
+
+// Mengambil nilai dari textarea dengan id "editor"
+var textarea = document.getElementById("editor");
+var teksYangDimasukkan = textarea.value;
+
+// Sekarang, Anda dapat menggunakan teksYangDimasukkan sesuai kebutuhan Anda
