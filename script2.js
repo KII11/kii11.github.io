d25e513d07e23c0efc4ea589296cdc73d79ae4e1
kii11 <riskiferdiansyah35@gmail.com>
2023-10-08 19:34:16 +0700

    2

--- script2.js
+++
@@ -1,5 +0,0 @@
-// Mengambil nilai dari textarea dengan id "editor" 
- var textarea = document.getElementById("editor"); 
- var teksYangDimasukkan = textarea.value; 
-  
- // Sekarang, Anda dapat menggunakan teksYangDimasukkan sesuai kebutuhan Anda
