e27d54223985a6db322e4d08d9ab3759e3ce50ad
riskifer <145009421+KII11@users.noreply.github.com>
2024-01-11 23:00:08 +0700

    kecepatan

--- beranda/matematika/script1.js
+++ beranda/matematika/script1.js
@@ -130,3 +130,16 @@ function toggleZoom() {
 
 const imageContainer = document.getElementById('imageContainer');
 imageContainer.addEventListener('click', toggleZoom);
+
+function toggleZoom() {
+    const zoomImage = document.getElementById('zoomImage');
+    isZoomed = !isZoomed;
+    if (isZoomed) {
+        zoomImage.classList.add('zoomed');
+    } else {
+        zoomImage.classList.remove('zoomed');
+    }
+}
+
+const imageContainer = document.getElementById('imageContainer');
+imageContainer.addEventListener('click', toggleZoom);
